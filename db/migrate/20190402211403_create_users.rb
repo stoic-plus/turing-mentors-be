e33532cfe8943b78f8196f97caf1531c01807490
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-03 13:32:11 -0600

    Revert "adds default value to user attributes"
    This reverts commit b742263ca1a4d2d910b12e6bb2175a77d0c5dacd.

--- db/migrate/20190402211403_create_users.rb
+++ db/migrate/20190402211403_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.integer :cohort
-      t.boolean :active, default: true
+      t.boolean :active
       t.string :program
       t.string :current_job
       t.text :background
