7cbc54259cd631a327ceaf0a48d1a45b1e29dca3
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-03 17:36:24 -0600

    adds spec for User.denver_mentors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,18 @@ describe User, type: :model do
   end
 
   describe 'class methods' do
+    context '.denver_mentors' do
+      it 'returns mentor users who are located in denver' do
+        u_1 = User.create(name: 'Travis Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: true, location: 'Denver, CO')
+        User.create(name: 'Ben Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: false, location: 'Denver, CO')
+        User.create(name: 'Frank Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: true, location: 'New York, NY')
 
+        actual = User.denver_mentors
+        expected = [u_1]
+
+        expect(actual).to eq(expected)
+      end
+    end
   end
 
   describe 'instance methods' do
