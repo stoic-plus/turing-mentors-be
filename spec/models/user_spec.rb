a6c21c57d2a0c8fb9fc5416a142fd0d47cfb03c6
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-02 18:34:38 -0600

    adds testing user relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,10 +11,13 @@ describe User, type: :model do
   end
 
   describe 'relationships' do
-    it {should have_many(:contact_details)}
+    it {should have_one(:contact_details)}
+    it {should have_many(:user_identities)}
     it {should have_many(:identities).through(:user_identities)}
+    it {should have_many(:user_tech_skills)}
     it {should have_many(:tech_skills).through(:user_tech_skills)}
-    it {should have_many(:nontech_skills).through(:user_nontech_skills)}
+    it {should have_many(:user_non_tech_skills)}
+    it {should have_many(:non_tech_skills).through(:user_non_tech_skills)}
   end
 
   describe 'class methods' do
