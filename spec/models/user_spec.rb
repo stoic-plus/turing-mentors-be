983670c0392a6e88627c0f193533f049b65424e0
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-03 17:39:15 -0600

    adds spec for User.mentors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,14 +20,24 @@ describe User, type: :model do
   end
 
   describe 'class methods' do
+    before :each do
+      @u_1 = User.create(name: 'Travis Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: true, location: 'Denver, CO')
+      @u_2 = User.create(name: 'Ben Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: false, location: 'Denver, CO')
+      @u_3 = User.create(name: 'Frank Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: true, location: 'New York, NY')
+    end
+
+    context '.mentors' do
+      it 'returns mentor users' do
+        actual = User.mentors
+        expected = [@u_1, @u_3]
+        expect(actual).to eq(expected)
+      end
+    end
+
     context '.denver_mentors' do
       it 'returns mentor users who are located in denver' do
-        u_1 = User.create(name: 'Travis Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: true, location: 'Denver, CO')
-        User.create(name: 'Ben Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: false, location: 'Denver, CO')
-        User.create(name: 'Frank Gee', cohort: 1810, program: 'FE', current_job: 'graduate', background: 'IT', mentor: true, location: 'New York, NY')
-
         actual = User.denver_mentors
-        expected = [u_1]
+        expected = [@u_1]
 
         expect(actual).to eq(expected)
       end
