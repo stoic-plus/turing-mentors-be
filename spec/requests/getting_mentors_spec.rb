572810832d076aa0ccc8bd0d3db22aaae711831d
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-03 17:31:57 -0600

    adds rest of test for GET mentors

--- spec/requests/getting_mentors_spec.rb
+++ spec/requests/getting_mentors_spec.rb
@@ -38,48 +38,148 @@ describe 'Getting Mentors', type: :request do
           expect(json["attributes"]).to have_key("cohort")
           expect(json["attributes"]).to have_key("background")
           expect(json["attributes"]).to have_key("location")
-          expect(json["attributes"]).to have_key("mentor")
+          expect(json["attributes"]["mentor"]).to be_truthy
         end
       end
     end
     context 'passing location:denver as params' do
       it 'returns mentor users in denver who have any tech skill' do
+        get '/api/v1/mentors', params: { location: 'denver' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(2)
+
+        mentor_json.each do |json|
+          expect(json["attributes"]["location"]).to eq("Denver, CO")
+          expect(json["attributes"]["mentor"]).to be_truthy
+        end
       end
     end
     context 'passing location:remote as params' do
       it 'returns mentor users outside of denver who have any tech skill' do
+        get '/api/v1/mentors', params: { location: 'remote' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(2)
+
+        mentor_json.each do |json|
+          expect(json["attributes"]["location"]).to eq("New York, NY")
+          expect(json["attributes"]["mentor"]).to be_truthy
+        end
       end
     end
     context 'passing location:all, tech_skills=\'javascript\' as params' do
       it 'returns mentor users in and outside of denver who have javascript as tech skill' do
+        get '/api/v1/mentors', params: { location: 'all', tech_skills: 'javascript' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(2)
+
+        mentor_json.each do |json|
+          expect(json["attributes"]["mentor"]).to be_truthy
+          expect(json["attributes"]["tech-skills"]).to include("javascript")
+        end
       end
     end
     context 'passing location:remote, tech_skills=\'javascript\' as params' do
       it 'returns mentor outside of denver who have javascript as tech skill' do
+        get '/api/v1/mentors', params: { location: 'remote', tech_skills: 'javascript' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(1)
+
+        expect(json["attributes"]["mentor"]).to be_truthy
+        expect(json["attributes"]["location"]).to_not eq("New York, NY")
+        expect(json["attributes"]["tech-skills"]).to include("javascript")
       end
     end
     context 'passing location:denver, tech_skills=\'javascript\' as params' do
       it 'returns mentor users in denver who have javascript as tech skill' do
+        get '/api/v1/mentors', params: { location: 'denver', tech_skills: 'javascript' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(1)
+
+        expect(json["attributes"]["mentor"]).to be_truthy
+        expect(json["attributes"]["location"]).to eq("Denver, CO")
+        expect(json["attributes"]["tech-skills"]).to include("javascript")
       end
     end
     context 'passing location:denver, tech_skills=\'ruby\' as params' do
       it 'returns mentor users in denver who have ruby as tech skill' do
+        get '/api/v1/mentors', params: { location: 'denver', tech_skills: 'ruby' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(1)
+
+        expect(json["attributes"]["mentor"]).to be_truthy
+        expect(json["attributes"]["location"]).to eq("Denver, CO")
+        expect(json["attributes"]["tech-skills"]).to include("ruby")
       end
     end
     context 'passing location:remote, tech_skills=\'ruby\' as params' do
       it 'returns mentor users not in denver who have ruby as tech skill' do
+        get '/api/v1/mentors', params: { location: 'remote', tech_skills: 'ruby' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(1)
+
+        expect(json["attributes"]["mentor"]).to be_truthy
+        expect(json["attributes"]["location"]).to eq("New York, NY")
+        expect(json["attributes"]["tech-skills"]).to include("ruby")
       end
     end
     context 'passing location:all, tech_skills=\'ruby\' as params' do
       it 'returns mentor users in and outside of denver who have ruby as tech skill' do
+        get '/api/v1/mentors', params: { location: 'all', tech_skills: 'ruby' }
 
+        expect(response).to be_successful
+        expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq(200)
+
+        mentor_json = JSON.parse(response.body)["data"]
+
+        expect(mentor_json.size).to eq(2)
+
+        mentor_json.each do |json|
+          expect(json["attributes"]["mentor"]).to be_truthy
+          expect(json["attributes"]["tech-skills"]).to include("ruby")
+        end
       end
     end
 end
