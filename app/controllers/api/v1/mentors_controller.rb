790638045b566e9b26e5522bf3c6670128e2c49b
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-04 11:09:08 -0600

    adds returning mentors by location, then by skills to mentors controller

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::MentorsController < ApplicationController
   def index
-    mentors = User.tech_skilled_in(params["tech_skills"]) if params["tech_skills"]
+    mentors = get_mentors_by_location(params["location"])
+    if params["tech_skills"]
+      binding.pry
+      mentors = mentors.tech_skilled_in(params["tech_skills"])
+    end
     render json: UserSerializer.new(mentors), status: 200
+  end
+
+  def get_mentors_by_location(location_param)
+    binding.pry
+    return User.mentors if location_param == "all"
+    return User.denver_mentors if location_param == "denver"
+    return User.remote_mentors if location_param == "remote"
   end
 end
