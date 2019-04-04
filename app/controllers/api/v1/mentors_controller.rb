3dca78128024afa12ec493b05a2226e34e178596
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-04 08:26:31 -0600

    adds remote_mentors scope to User, more seeds

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MentorsController < ApplicationController
   def index
-    mentors = User.all
+    mentors = User.tech_skilled_in(params["tech_skills"]) if params["tech_skills"]
     render json: UserSerializer.new(mentors), status: 200
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   scope :mentors, -> { where(mentor: true) }
   scope :denver_mentors, -> { mentors.where(location: "Denver, CO") }
-
+  scope :remote_mentors, -> { mentors.where("location != 'Denver, CO'") }
   validates_presence_of :name,
                         :current_job,
                         :active,
@@ -14,4 +14,15 @@ class User < ApplicationRecord
   has_many :tech_skills, through: :user_tech_skills
   has_many :user_non_tech_skills
   has_many :non_tech_skills, through: :user_non_tech_skills
+
+  def self.tech_skilled_in(languages)
+     # SELECT name, title FROM users INNER JOIN user_tech_skills ON user_tech_skills.user_id=users.id INNER JOIN tech_skills ON user_tech_skills.tech_skill_id=tech_skills.id;
+#      name    |   title
+# ------------+------------
+# Travis Gee | Ruby
+# Bob Gee    | Ruby
+# Jordan Gee | Javascript
+# J J        | Javascript
+# J J        | Ruby
+  end
 end
