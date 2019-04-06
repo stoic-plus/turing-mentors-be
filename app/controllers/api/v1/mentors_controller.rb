449d922aef55869a6bb8fd8eb8039a3d6861728e
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-06 11:52:20 -0600

    adds creating mentor availability in mentors#create

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -16,9 +16,9 @@ class Api::V1::MentorsController < ApplicationController
       program: mentor_attributes[:program],
       background: mentor_attributes[:background]
     }
-    user = User.new(user_params)
+    mentor = User.new(user_params)
 
-    if user.save
+    if mentor.save
       contact_info = {
         email: mentor_attributes[:email],
         slack: mentor_attributes[:slack],
@@ -28,7 +28,16 @@ class Api::V1::MentorsController < ApplicationController
       mentor_attributes[:identities].each do |identity_id|
         UserIdentity.create(user: user, identity: identity_id)
       end
-
+      mentor_attributes[:availability].each do |day, time_of_day|
+        morning, afternoon, evening = time_of_day
+        Availability.create(
+          day_of_week: day,
+          morning: morning,
+          afternoon: afternoon,
+          evening: evening
+        )
+      end
+      render json: UserSerializer.new(mentor), status: 200
     else
       render json: { message: "incorrect user information supplied"}, status: 400
     end
