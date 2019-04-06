336f817931afcaf3a9527098c244a7a816a848d2
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-06 11:29:12 -0600

    adds POST mentors route and create action in controller

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -6,4 +6,55 @@ class Api::V1::MentorsController < ApplicationController
     end
     render json: UserSerializer.new(mentors), status: 200
   end
+
+  def create
+    mentor_attributes = mentor_params
+    user_params = {
+      first_name: mentor_attributes[:firstName],
+      last_name: mentor_attributes[:lastName],
+      cohort: mentor_attributes[:cohort],
+      program: mentor_attributes[:program],
+      background: mentor_attributes[:background]
+    }
+    user = User.new(user_params)
+
+    if user.save
+      contact_info = {
+        email: mentor_attributes[:email],
+        slack: mentor_attributes[:slack],
+        phone: mentor_attributes[:phone]
+      }
+      ContactDetails.create(contact_info)
+      mentor_attributes[:identities].each do |identity_id|
+        UserIdentity.create(user: user, identity: identity_id)
+      end
+
+    else
+      render json: { message: "incorrect user information supplied"}, status: 400
+    end
+  end
+
+  private
+
+  def mentor_params
+    params.permit(
+    :background,
+    :cohort,
+    :program,
+    :email,
+    :firstName,
+    :tech_skills,
+    :identities,
+    :lastName,
+    :phone,
+    :slack,
+    :timeFri,
+    :timeMon,
+    :timeSat,
+    :timeSun,
+    :timeThu,
+    :timeTue,
+    :timeWed
+  )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/mentors', to: 'mentors#index'
+      post '/mentors', to: 'mentors#create'
     end
   end
 end
+ 
