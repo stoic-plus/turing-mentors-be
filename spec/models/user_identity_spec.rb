2b8c8c4752ddaa7698c520e880cc8fa32dba243f
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-09 15:34:49 -0600

    adds UserIdentity.for_user - creates rows on itself given identity_ids and a user

--- spec/models/user_identity_spec.rb
+++ spec/models/user_identity_spec.rb
@@ -4,9 +4,9 @@ describe UserIdentity, type: :model do
   describe 'class methods' do
     context 'for_user' do
       it 'creates identities for a user given array of identity id\'s and a user' do
-        id_1 = UserIdentity.create(title: 'male')
-        id_2 = UserIdentity.create(title: 'parent')
-        id_3 = UserIdentity.create(title: 'trucker')
+        id_1 = Identity.create(title: 'male')
+        id_2 = Identity.create(title: 'parent')
+        id_3 = Identity.create(title: 'trucker')
         user = User.create(first_name: 'Travis', last_name: ' Gee', cohort: 1810, program: 'FE', current_job: 'google', background: 'IT', mentor: true, location: 'Denver, CO')
         UserIdentity.for_user([1, 2, 3], user)
 
