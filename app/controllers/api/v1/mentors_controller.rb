36529a7efb44c8b2d1dcdccef4bcfe0735dfc89f
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-09 16:50:31 -0600

    refactors MC#create to allow location and use User.new_mentor

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -8,23 +8,13 @@ class Api::V1::MentorsController < ApplicationController
   end
 
   def create
-    mentor_attributes = mentor_params
-    user_params = {
-      first_name: mentor_attributes[:first_name],
-      last_name: mentor_attributes[:last_name],
-      current_job: mentor_attributes[:current_job],
-      cohort: mentor_attributes[:cohort],
-      program: mentor_attributes[:program],
-      background: mentor_attributes[:background],
-      mentor: true
-    }
-    mentor = User.new(user_params)
+    mentor = User.new_mentor(mentor_params)
     if mentor.save
-      ContactDetails.for_user(mentor_attributes, mentor)
-      UserIdentity.for_user(mentor_attributes[:identities].map(&:to_i), mentor)
-      UserTechSkill.for_user(mentor_attributes[:tech_skills].map(&:to_i), mentor)
-      UserNonTechSkill.for_user(mentor_attributes[:non_tech_skills].map(&:to_i), mentor)
-      Availability.for_user(mentor_attributes[:availability], mentor)
+      ContactDetails.for_user(mentor_params, mentor)
+      UserIdentity.for_user(mentor_params[:identities].map(&:to_i), mentor)
+      UserTechSkill.for_user(mentor_params[:tech_skills].map(&:to_i), mentor)
+      UserNonTechSkill.for_user(mentor_params[:non_tech_skills].map(&:to_i), mentor)
+      Availability.for_user(mentor_params[:availability], mentor)
       render json: UserSerializer.new(mentor), status: 200
     else
       render json: { message: "incorrect user information supplied"}, status: 400
@@ -38,6 +28,7 @@ class Api::V1::MentorsController < ApplicationController
     :background,
     :cohort,
     :program,
+    :location,
     :current_job,
     :email,
     :first_name,
