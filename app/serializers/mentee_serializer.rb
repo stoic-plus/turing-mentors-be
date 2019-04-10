6668c625e87a98a0bd32fc129d7d8692f14a6a3a
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-09 18:12:32 -0600

    adds mentee_serializer, changes user to mentor serializer

---
+++ app/serializers/mentee_serializer.rb
@@ -0,0 +1,13 @@
+class MenteeSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :first_name, :last_name, :cohort, :program, :background, :mentor
+  attribute :availability do |user|
+    user.list_availability
+  end
+  attribute :identities do |user|
+    user.list_identities
+  end
+  attribute :contact_details do |user|
+    user.list_contact_details
+  end
+end
