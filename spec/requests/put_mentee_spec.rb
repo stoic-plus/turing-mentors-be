16ecdcad8f7dc9fcf92b43ea3499cf82df5dfe03
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-21 10:16:36 -0600

    fixes expects in test, adds testing for updating identities

--- spec/requests/put_mentee_spec.rb
+++ spec/requests/put_mentee_spec.rb
@@ -5,6 +5,9 @@ describe 'PUT /mentees', type: :request do
     i_1 = Identity.create(title: 'male')
     i_2 = Identity.create(title: 'parent')
     i_3 = Identity.create(title: 'pianist')
+    i_4 = Identity.create(title: 'biker')
+    i_5 = Identity.create(title: 'surfer')
+    i_6 = Identity.create(title: 'renegade')
     @user = User.create(
       background: 'a',
       cohort: 1810,
@@ -32,14 +35,24 @@ describe 'PUT /mentees', type: :request do
         phone: "510",
         slack: "@burgerzBoss",
         availability: {
-          0 => [false, false, false],
-          1 => false,
-          2 => [false, false, false],
-          3 => [false, false, false],
-          4 => [false, false, false],
-          5 => [false, false, false],
-          6 => [false, false, false]
+          "0" => false,
+          "1" => false,
+          "2" => [true, false, false],
+          "3" => [true, false, false],
+          "4" => [true, false, false],
+          "5" => [true, false, false],
+          "6" => true
         }
+      }
+
+      return_availability = {
+        "0" => [false, false, false],
+        "1" => [false, false, false],
+        "2" => [true, false, false],
+        "3" => [true, false, false],
+        "4" => [true, false, false],
+        "5" => [true, false, false],
+        "6" => [true, true, true]
       }
 
       put "/api/v1/mentees/#{@user.id}", params: updated
@@ -47,17 +60,16 @@ describe 'PUT /mentees', type: :request do
       expect(response.status).to eq(200)
       expect(response).to be_successful
       returned_user = JSON.parse(response.body)["data"]["attributes"]
-
-      expect(returned_user[background]).to eq(@user[:background])
-      expect(returned_user[cohort]).to eq(@user[:cohort])
-      expect(returned_user[program]).to eq(@user[:program])
-      expect(returned_user[email]).to eq(@user[:email])
-      expect(returned_user[first_name]).to eq(@user[:first_name])
-      expect(returned_user[identities]).to eq(@user[:identities])
-      expect(returned_user[last_name]).to eq(@user[:last_name])
-      expect(returned_user[phone]).to eq(updated[:phone])
-      expect(returned_user[slack]).to eq(updated[:slack])
-      expect(returned_user[availability]).to eq(updated[:availability])
+      expect(returned_user["background"]).to eq(@user[:background])
+      expect(returned_user["cohort"]).to eq(@user[:cohort])
+      expect(returned_user["program"]).to eq(@user[:program])
+      expect(returned_user["email"]).to eq(@user[:email])
+      expect(returned_user["first_name"]).to eq(@user[:first_name])
+      expect(returned_user["identities"]).to eq(@user.identities.pluck(:title))
+      expect(returned_user["last_name"]).to eq(@user[:last_name])
+      expect(returned_user["contact_details"]["phone"]).to eq(updated[:phone])
+      expect(returned_user["contact_details"]["slack"]).to eq(updated[:slack])
+      expect(returned_user["availability"]).to eq(return_availability)
     end
   end
 
@@ -74,13 +86,13 @@ describe 'PUT /mentees', type: :request do
         phone: "8",
         slack: "@slackville",
         availability: {
-          0 => [true, true, true],
-          1 => true,
-          2 => [true, true, true],
-          3 => [true, true, true],
-          4 => [true, true, true],
-          5 => [true, true, true],
-          6 => [true, true, true]
+          "0" => [true, true, true],
+          "1" => [false, false, false],
+          "2" => [true, true, true],
+          "3" => [true, true, true],
+          "4" => [true, true, true],
+          "5" => [true, true, true],
+          "6" => [true, true, true]
         }
       }
 
@@ -90,16 +102,16 @@ describe 'PUT /mentees', type: :request do
       expect(response).to be_successful
       returned_user = JSON.parse(response.body)["data"]["attributes"]
 
-      expect(returned_user[background]).to eq(updated[:background])
-      expect(returned_user[cohort]).to eq(updated[:cohort])
-      expect(returned_user[program]).to eq(updated[:program])
-      expect(returned_user[email]).to eq(updated[:email])
-      expect(returned_user[first_name]).to eq(updated[:first_name])
-      expect(returned_user[identities]).to eq(updated[:identities])
-      expect(returned_user[last_name]).to eq(updated[:last_name])
-      expect(returned_user[phone]).to eq(updated[:phone])
-      expect(returned_user[slack]).to eq(updated[:slack])
-      expect(returned_user[availability]).to eq(updated[:availability])
+      expect(returned_user["background"]).to eq(updated[:background])
+      expect(returned_user["cohort"]).to eq(updated[:cohort])
+      expect(returned_user["program"]).to eq(updated[:program])
+      expect(returned_user["contact_details"]["email"]).to eq(updated[:email])
+      expect(returned_user["first_name"]).to eq(updated[:first_name])
+      expect(returned_user["identities"]).to eq(Identity.pluck(:title))
+      expect(returned_user["last_name"]).to eq(updated[:last_name])
+      expect(returned_user["contact_details"]["phone"]).to eq(updated[:phone])
+      expect(returned_user["contact_details"]["slack"]).to eq(updated[:slack])
+      expect(returned_user["availability"]).to eq(updated[:availability])
     end
   end
 
