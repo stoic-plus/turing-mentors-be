54f41b05ece73cf972bcb9a3acdb2d71faed1383
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-22 14:42:55 -0600

    adds joins table model methods to update skills for a mentor user

--- app/models/user_non_tech_skill.rb
+++ app/models/user_non_tech_skill.rb
@@ -7,4 +7,10 @@ class UserNonTechSkill < ApplicationRecord
       create(non_tech_skill_id: id, user: user)
     end
   end
+
+  def self.update_for_user(user, skill_params)
+    current_skills = user.non_tech_skills.pluck(:id)
+    new_skills = skill_params.map(&:to_i).select {|non_tech_skill| !current_skills.include?(non_tech_skill)}
+    new_skills.each{|non_tech_skill| self.create(user_id: user.id, non_tech_skill_id: non_tech_skill)}
+  end
 end
--- app/models/user_tech_skill.rb
+++ app/models/user_tech_skill.rb
@@ -7,4 +7,10 @@ class UserTechSkill < ApplicationRecord
       create(tech_skill_id: id, user: user)
     end
   end
+
+  def self.update_for_user(user, skill_params)
+    current_skills = user.tech_skills.pluck(:id)
+    new_skills = skill_params.map(&:to_i).select {|tech_skill| !current_skills.include?(tech_skill)}
+    new_skills.each{|tech_skill| self.create(user_id: user.id, tech_skill_id: tech_skill)}
+  end
 end
