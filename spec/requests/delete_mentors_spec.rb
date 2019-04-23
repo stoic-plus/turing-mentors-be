c124dc230761eef00b1714f7153b5e0e9bd2b317
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-23 17:11:16 -0600

    adds spec for delete mentors

--- spec/requests/delete_mentors_spec.rb
+++ spec/requests/delete_mentors_spec.rb
@@ -2,16 +2,24 @@ require 'rails_helper'
 
 describe 'DELETE /mentors', type: :request do
   before :each do
-    i_1 = Identity.create(title: 'male')
+    @i_1 = Identity.create(title: 'female')
     @user = User.create(
       background: 'a',
       cohort: 1810,
       program: "BE",
       first_name: "Jordan",
       last_name: "l",
+      current_job: "Mcdonalds",
+      location: "Atlanta",
+      mentor: true
     )
-    UserIdentity.create(user: @user, identity_id: i_1.id)
-    ContactDetails.create(email: "mail",phone:"2",slack:"@slack", user: @user)
+    @ts_1 = TechSkill.create(title: 'javascript')
+    @nts_1 = NonTechSkill.create(title: 'stress management')
+    UserIdentity.create(user: @user, identity_id: @i_1.id)
+    UserTechSkill.create(user: @user, tech_skill_id: @ts_1.id)
+    UserNonTechSkill.create(user: @user, non_tech_skill_id: @nts_1.id)
+
+    @contact = ContactDetails.create(email: "mail",phone:"2",slack:"@slack", user: @user)
     Availability.create(day_of_week: 0, morning: false, afternoon: false, evening: true, user: @user)
   end
 
@@ -19,16 +27,20 @@ describe 'DELETE /mentors', type: :request do
     it 'removes the user and all entries in supporting tables' do
       expect(User.count).to eq(1)
       expect(UserIdentity.count).to eq(1)
+      expect(UserTechSkill.count).to eq(1)
+      expect(UserNonTechSkill.count).to eq(1)
       expect(ContactDetails.count).to eq(1)
       expect(Availability.count).to eq(1)
 
-      delete "/api/v1/mentees/#{@user.id}"
+      delete "/api/v1/mentors/#{@user.id}"
 
       expect(response.status).to eq(204)
       expect(response).to be_successful
 
       expect(User.count).to eq(0)
       expect(UserIdentity.count).to eq(0)
+      expect(UserTechSkill.count).to eq(0)
+      expect(UserNonTechSkill.count).to eq(0)
       expect(ContactDetails.count).to eq(0)
       expect(Availability.count).to eq(0)
     end
