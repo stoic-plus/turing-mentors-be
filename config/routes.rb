f55a2cdfa467f1bbd0485334fd072eed6bf5a92b
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-23 17:04:01 -0600

    adds delete route and dependent destroys to User

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :mentors, only: [:index, :create, :update]
-      resources :mentees, only: [:update, :create]
+      resources :mentees, only: [:update, :create, :destroy]
     end
   end
 end
