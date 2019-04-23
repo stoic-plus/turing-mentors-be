539a2b0054e30a202abeacf473c0f7243526746a
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-23 17:25:56 -0600

    adds destroy route to mentorscontr

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :mentors, only: [:index, :create, :update]
+      resources :mentors, only: [:index, :create, :update, :destroy]
       resources :mentees, only: [:update, :create, :destroy]
     end
   end
