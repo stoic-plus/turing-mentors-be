c36d94277475c37b951dc57b337875b863688493
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-24 18:05:02 -0600

    adds spec for not deleting a mentor or mentee if valid id passed to each endpoint

--- spec/requests/delete_mentee_spec.rb
+++ spec/requests/delete_mentee_spec.rb
@@ -9,6 +9,7 @@ describe 'DELETE /mentees', type: :request do
       program: "BE",
       first_name: "Jordan",
       last_name: "l",
+      mentor: false
     )
     UserIdentity.create(user: @user, identity_id: i_1.id)
     ContactDetails.create(email: "mail",phone:"2",slack:"@slack", user: @user)
@@ -16,6 +17,45 @@ describe 'DELETE /mentees', type: :request do
   end
 
   context 'passing valid id' do
+    it 'does not remove user if found user is a mentor' do
+      user = User.create(
+        background: 'a',
+        cohort: 1810,
+        program: "BE",
+        first_name: "Jordan",
+        last_name: "l",
+        current_job: "Mcdonalds",
+        location: "Atlanta",
+        mentor: true
+      )
+      TechSkill.create(title: 'javascript')
+      NonTechSkill.create(title: 'stress management')
+      UserIdentity.create(user: @user, identity_id: @i_1.id)
+      UserTechSkill.create(user: @user, tech_skill_id: @ts_1.id)
+      UserNonTechSkill.create(user: @user, non_tech_skill_id: @nts_1.id)
+      ContactDetails.create(email: "mail",phone:"2",slack:"@slack", user: @user)
+      Availability.create(day_of_week: 0, morning: false, afternoon: false, evening: true, user: @user)
+
+      expect(User.count).to eq(2)
+      expect(UserIdentity.count).to eq(2)
+      expect(ContactDetails.count).to eq(2)
+      expect(Availability.count).to eq(2)
+      expect(UserTechSkill.count).to eq(1)
+      expect(UserNonTechSkill.count).to eq(1)
+
+      delete "/api/v1/mentees/#{user.id}"
+
+      expect(response.status).to eq(404)
+      expect(JSON.parse(response.body)).to eq({"message" => "mentee not found by that id"})
+
+      expect(User.count).to eq(2)
+      expect(UserIdentity.count).to eq(2)
+      expect(ContactDetails.count).to eq(2)
+      expect(Availability.count).to eq(2)
+      expect(UserTechSkill.count).to eq(1)
+      expect(UserNonTechSkill.count).to eq(1)
+    end
+
     it 'removes the user and all entries in supporting tables' do
       expect(User.count).to eq(1)
       expect(UserIdentity.count).to eq(1)
@@ -26,7 +66,7 @@ describe 'DELETE /mentees', type: :request do
 
       expect(response.status).to eq(204)
       expect(response).to be_successful
-      
+
       expect(User.count).to eq(0)
       expect(UserIdentity.count).to eq(0)
       expect(ContactDetails.count).to eq(0)
