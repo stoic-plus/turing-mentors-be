e3e2268fec1e9e9a83b94f5a36ab5da4d669cd67
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-27 14:43:47 -0600

    Adds more expect for supporting info for mentee to spec

--- spec/requests/get_mentees_spec.rb
+++ spec/requests/get_mentees_spec.rb
@@ -10,8 +10,8 @@ describe 'GET /mentees', type: :request do
 
     @user_2 = User.create(background: 'a',cohort: 1811,program: "FE",first_name: "Jay",last_name: "l")
     UserIdentity.create(user: @user_2, identity_id: @i_1.id)
-    ContactDetails.create(email: "mail",phone:"2",slack:"@slack", user: @user)
-    Availability.create(day_of_week: 0, morning: false, afternoon: false, evening: true, user: @user)
+    @contact = ContactDetails.create(email: "mail",phone:"2",slack:"@slack", user: @user_2)
+    @availability = Availability.create(day_of_week: 0, morning: false, afternoon: false, evening: true, user: @user_2)
   end
 
   context 'base route with no parameters' do
@@ -24,18 +24,25 @@ describe 'GET /mentees', type: :request do
       mentee_json = JSON.parse(response.body)["data"]
       mentees = [@user, @user_2]
 
-      expect(mentor_json.size).to eq(2)
+      expect(mentee_json.size).to eq(2)
 
       mentee_json.each_with_index do |json, i|
         expect(json).to have_key("id")
         expect(json["type"]).to eq("mentee")
-        expect(json["first_name"]).to eq(mentees[i].first_name)
-        expect(json["last_name"]).to eq(mentees[i].last_name)
-        expect(json["cohort"]).to eq(mentees[i].cohort)
-        expect(json["program"]).to eq(mentees[i].program)
-        expect(json["background"]).to eq(mentees[i].background)
-        expect(json["location"]).to eq(mentees[i].location)
+        expect(json["attributes"]["first_name"]).to eq(mentees[i].first_name)
+        expect(json["attributes"]["last_name"]).to eq(mentees[i].last_name)
+        expect(json["attributes"]["cohort"]).to eq(mentees[i].cohort)
+        expect(json["attributes"]["program"]).to eq(mentees[i].program)
+        expect(json["attributes"]["background"]).to eq(mentees[i].background)
         expect(json["attributes"]["mentor"]).to be_falsey
+        expect(json["attributes"]["availability"].first.first.to_i).to eq(@availability.day_of_week)
+        expect(json["attributes"]["availability"].first.second.first).to eq(@availability.morning)
+        expect(json["attributes"]["availability"].first.second.second).to eq(@availability.afternoon)
+        expect(json["attributes"]["availability"].first.second.third).to eq(@availability.evening)
+        expect(json["attributes"]["identities"]).to eq([@i_1.title])
+        expect(json["attributes"]["contact_details"]["email"]).to eq(@contact.email)
+        expect(json["attributes"]["contact_details"]["slack"]).to eq(@contact.slack)
+        expect(json["attributes"]["contact_details"]["phone"]).to eq(@contact.phone)
       end
     end
   end
